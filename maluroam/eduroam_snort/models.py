3f8551ba1fa3b8ee97e44f2accd55f73a00b3aa8
Thomas Grainger <tagrain@gmail.com>
2012-08-01 13:54:10 +0100

    add django project shell

---
+++ maluroam/eduroam_snort/models.py
@@ -0,0 +1,52 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#     * Rearrange models' order
+#     * Make sure each model has one field with primary_key=True
+# Feel free to rename the models, but don't rename db_table values or field names.
+#
+# Also note: You'll have to insert the output of 'django-admin.py sqlcustom [appname]'
+# into your database.
+
+from django.db import models
+
+class Blacklists(models.Model):
+    bl_id = models.BigIntegerField(primary_key=True)
+    name = models.CharField(max_length=765)
+    url = models.CharField(max_length=765)
+    serialized = models.TextField()
+    updated = models.DateTimeField()
+    hide = models.IntegerField()
+    class Meta:
+        db_table = u'blacklists'
+
+class Event(models.Model):
+    event_id = models.BigIntegerField(primary_key=True)
+    username = models.CharField(max_length=765)
+    radius_account_id = models.CharField(max_length=765)
+    radius_session_id = models.CharField(max_length=765)
+    radius_info = models.TextField()
+    ip_src = models.CharField(max_length=765)
+    ip_dst = models.CharField(max_length=765)
+    start = models.DateTimeField(unique=True)
+    finish = models.DateTimeField(unique=True)
+    alerts = models.BigIntegerField()
+    blacklist = models.IntegerField()
+    rule = models.BigIntegerField()
+    rule_class = models.CharField(max_length=93)
+    class Meta:
+        db_table = u'event'
+
+class Rules(models.Model):
+    rule_id = models.BigIntegerField(primary_key=True)
+    rule_name = models.CharField(max_length=765)
+    hide = models.IntegerField()
+    class Meta:
+        db_table = u'rules'
+
+class Scripts(models.Model):
+    script_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=765)
+    lastupdated = models.DateTimeField()
+    class Meta:
+        db_table = u'scripts'
+
