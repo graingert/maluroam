c3a50672f62c5f44560ed6af6b69587b3168a4ba
Thomas Grainger <tagrain@gmail.com>
2012-08-06 17:24:12 +0100

    added php code to views to convert to django

--- maluroam/eduroam_snort/views.py
+++ maluroam/eduroam_snort/views.py
@@ -130,3 +130,215 @@ def dashboard(request):
 #}
 
 #?>
+
+def getOverview(start, finish, username=None):
+    delta = end - finish;
+        
+    if delta <= timedelta(days=4):
+        interval = timedelta(hour=1)
+    else if delta <= timedelta(months=3):
+        interval = timedelta(day=1)
+    else if delta <= timedelta(years = 3):
+        interval = timedelta(month=1)
+    else:
+        interval = timedelta(year=1)
+        
+    events = Event.objects.filter(
+        Q(rule__hide=False) | Q(blacklist__hide=False),
+        start__gte = start,
+        finish__lte = finish,
+    )
+    
+    events = events.values("blacklist", "rule").annotate(Count('event_id'), Sum("alerts"))
+    
+    if username:
+        events = events.filter(username=username)
+    
+"""
+        SELECT DATE_FORMAT(e.start, '{dateformat}') as timestamp, COUNT(e.event_id) as alerts, bl.name as blacklist, rule, rule_name, rule_class, COUNT(DISTINCT(e.username)) as users
+        FROM event e
+        LEFT JOIN blacklists bl
+            ON e.blacklist = bl.bl_id
+        LEFT JOIN rules r
+            ON r.rule_id = e.rule
+        WHERE
+            e.start >= '{from}'
+            AND e.finish <= '{to}'
+            AND (r.hide = 0 OR bl.hide = 0)
+            {user_where}
+        GROUP BY DATE_FORMAT(e.start, {dateformat}), bl.bl_id, rule
+        ORDER BY rule ASC, bl.name ASC, e.start ASC
+"""
+
+
+public function getOverview($from, $to, $username=""){
+		
+		// Work out the length of time between two dates
+		$diff = strtotime($to) - strtotime($from);
+		
+		// Work out the date format, SQL date format and interval
+		// period between the points.
+		// ---
+		// If under 4 days, show hourly intervals
+		if ($diff <= (60*60*24*4)){
+			$dateformat = 'Y-m-d H:00:00';
+			$sqldateformat = '%Y-%m-%d %H:00:00';
+			$interval = "1 hour";
+		// If under 3 months, show daily intervals
+		} elseif ($diff <= (60*60*24*31*3)) {
+			$dateformat = 'Y-m-d';
+			$sqldateformat = '%Y-%m-%d';
+			$interval = "1 day";
+		// If under 3 years, show monthly intervals
+		} elseif ($diff <= (60*60*24*31*12*3)) {
+			$dateformat = 'Y-m';
+			$sqldateformat = '%Y-%m';
+			$interval = "1 month";
+		// Else yearly intervals
+		} else {
+			$dateformat = 'Y';
+			$sqldateformat = '%Y';
+			$interval = "1 year";
+		}
+		
+		// Set start and end times, in timestamp form
+		$start = strtotime( date($dateformat, strtotime($from) ) );
+		$end = strtotime( date($dateformat, strtotime($to) ) );
+		
+		// Add SQL WHERE clause for username, if one was passed into the argument
+		$user_where = ($username != "")
+			? sprintf("AND username = '%s'", $username)
+			: "";
+		
+		// Generate required SQL query for data
+		$sql = sprintf("
+			SELECT DATE_FORMAT(e.start, '%s') as timestamp, COUNT(e.event_id) as alerts, bl.name as blacklist, rule, rule_name, rule_class, COUNT(DISTINCT(e.username)) as users
+			FROM event e
+			LEFT JOIN blacklists bl
+				ON e.blacklist = bl.bl_id
+			LEFT JOIN rules r
+				ON r.rule_id = e.rule
+			WHERE
+				e.start >= '%s'
+				AND e.finish <= '%s'
+				AND (r.hide = 0 OR bl.hide = 0)
+				%s
+			GROUP BY DATE_FORMAT(e.start, '%s'), bl.bl_id, rule
+			ORDER BY rule ASC, bl.name ASC, e.start ASC
+		", $sqldateformat, $from, $to, $user_where, $sqldateformat);
+		
+		// Fetch result
+		$rst = mysql_query($sql);
+		$grouping = array();
+		
+		// If a result exists, loop through results
+		if($rst){
+			while($row = mysql_fetch_assoc($rst)){
+				// Convert datetime to timestamp
+				$row['timestamp'] = strtotime($row['timestamp']);
+				
+				// Determine if event is a blacklist or rule, and if a rule
+				// check whether a rule name has been set for it. Store events
+				// in an array timestamp=>alertinfo
+				if(!is_null($row['blacklist']) || $row['blacklist'] > 0){
+					$grouping[$row['blacklist']][$row['timestamp']] = $row['alerts'];
+				} elseif(!is_null($row['rule_name']) && $row['rule_name'] != ''){
+					$grouping[$row['rule_name'].'['.$row['rule'].']'][$row['timestamp']] = $row['alerts'];
+				} else {
+					$grouping['snort_' . $row['rule']][$row['timestamp']] = $row['alerts'];
+				}
+			}
+		} else {
+			// Else chuck out errors
+			mysql_error();
+		}
+		
+		// Unset MySQL result, no longer needed
+		unset($rst);
+		
+		// Loop through each alert/blacklist group of alerts
+		foreach($grouping as $key => $group){
+			// Work out the total number of alerts within current group
+			$totals[$key] = array_sum($group);
+			
+			$unset = 0;
+			
+			// Loop through each alert
+			foreach($group as $timestamp => $alert){
+				// Set dates for previous, current and next points
+				$date = $timestamp;
+				$prev = strtotime(date($dateformat, $date) . ' -' . $interval);
+				$next = strtotime(date($dateformat, $date) . ' +' . $interval);
+				
+				// Check previous interval, set to 0 if not set
+				if ( !isset( $grouping[$key][$prev] ) && $prev > $start){
+					if($unset == 0){
+						$grouping[$key][$prev] = 0;
+						$set = true;
+					}
+				}
+				
+				// Check next interval, set to 0 if not set
+				if ( !isset( $grouping[$key][$next] ) && $next < $end){
+					$grouping[$key][$next] = 0;
+					$unset = 0;
+					$set = true;
+				}
+				
+				// If previous and next points have been set...
+				if(!$set){
+					// If 3 points in a row are the same
+					if( $grouping[$key][$prev] == $grouping[$key][$next] && $grouping[$key][$prev] == $alert){
+						// unset current point
+						unset($grouping[$key][$timestamp]);
+						$unset = $grouping[$key][$prev];
+					} elseif($unset == $grouping[$key][$next] && $unset == $alert ){
+						// unset current point
+						unset($grouping[$key][$timestamp]);
+					} else {
+						$unset = 0;
+					}
+				}
+				
+				// Reset the set variable
+				$set = false;
+			}
+
+			// Add starting point
+			if(!isset($grouping[$key][$start])){
+				$grouping[$key][$start] = 0;
+			}
+			
+			// Add ending point
+			$checkend = ($interval == "1 hour")
+				? ($end - 3600)
+				: $end;
+			
+			// Add end point if not set
+			if(!isset($grouping[$key][$checkend]) ){
+				$grouping[$key][$checkend] = 0;
+			}
+			
+			// Sort array by key value (timestamp)
+			ksort($grouping[$key]);
+		}
+		
+		// Loop through entire arary again, offsetting timestamps for Flot
+		// which only accepts UTC timestamps!
+		foreach($grouping as $key => $group){
+			foreach($group as $time => $alerts){
+				$return[] = '[' . (($time+3600)*1000) . ',' . $alerts . ']';
+			}
+			
+			// Generate information for rule/blacklist
+			$grouping[$key] = array(
+				'name' => $key,
+				'json' => '[' . implode(', ', $return) . ']',
+				'total' => $totals[$key]
+			);
+			unset($return);
+		}
+		
+		// Return data
+		return $grouping;
+	}
