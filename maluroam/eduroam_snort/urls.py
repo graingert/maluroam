b63d3d4a3051daa3cad8bc9452b8e16fba6998c1
Thomas Grainger <tagrain@gmail.com>
2012-08-10 14:54:39 +0100

    add urls for other pages, to allow the url templatetag to operate

--- maluroam/eduroam_snort/urls.py
+++ maluroam/eduroam_snort/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.base import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -6,7 +7,22 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('eduroam_snort.views',
     # Examples:
-    url(r'^dashboard/$', 'dashboard', name='dashboard'),
+    url(r'^$', 'dashboard', name='dashboard'),
     url(r'^overviews.json$', 'overview', name="overviews"),
-    url(r'^user/(?P<slug>[\w-]+)$', 'user', name="user"),
+    
+    url(
+        r'^user/(?P<slug>[\w-]+)$',
+        TemplateView.as_view(template_name="eduroam_snort/user.html"),
+        name="user"
+    ),
+    url(
+        r'^user/$',
+        TemplateView.as_view(template_name="eduroam_snort/users.html"),
+        name="users"
+    ),
+    url(
+        r'^settings/$',
+        TemplateView.as_view(template_name="eduroam_snort/settings.html"),
+        name="settings"
+    ),
 )
