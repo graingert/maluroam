9054e38064d9ed74a9a950728744bedcc28a48f6
Thomas Grainger <tagrain@gmail.com>
2012-08-14 10:36:31 +0100

    fix settings page to use bootstrap tables

--- maluroam/eduroam_snort/static/eduroam_snort/js/misc.js
+++ maluroam/eduroam_snort/static/eduroam_snort/js/misc.js
@@ -1,21 +1,17 @@
 $(document).ready(function(){
-
-		$('#usersearch').autocomplete({
-			source: "/index.php?page=users&action=search",
-			minLength: 2,
-			select: function( event, ui ) {
-				if( ui.item ){
-					var href = "/index.php?page=user&user=" + ui.item.value;
-					window.location.href = href;
-				}
-			}
-		});
-		
-		$('#date-search .button').button();
-		$('#date-search #searchfrom, #date-search #searchto').datetimepicker({
-			dateFormat: 'yy-mm-dd'
-		});
-		
-        $('#tabs').tabs();
-        $('.button').button();
+    $('#usersearch').autocomplete({
+        source: "/index.php?page=users&action=search",
+        minLength: 2,
+        select: function( event, ui ) {
+            if( ui.item ){
+                var href = "/index.php?page=user&user=" + ui.item.value;
+                window.location.href = href;
+            }
+        }
+    });
+    
+    $('#date-search .button').button();
+    $('#date-search #searchfrom, #date-search #searchto').datetimepicker({
+        dateFormat: 'yy-mm-dd'
+    });
 });
