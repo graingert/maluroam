5264e4de7bc9a99a70355c6f47a591ccf247417f
Thomas Grainger <tagrain@gmail.com>
2012-08-24 11:21:52 +0100

    remove animations - not useful slowed down interface

--- maluroam/eduroam_snort/static/eduroam_snort/js/pjax-paginator.js
+++ maluroam/eduroam_snort/static/eduroam_snort/js/pjax-paginator.js
@@ -1,13 +1,5 @@
 $(function () {
     "use strict";
     $('#paginate a').pjax('#paginate', {"fragment":"#paginate", "scrollTo":false});
-    
-    $('#paginate').on('pjax:beforeSend', function(){
-        $('#paginate').hide('slow');
-    });
-    
-    $('#paginate').on('pjax:end', function(){
-        $('#paginate').show('slow');
-    });
     //$('a:not(#paginate a)').pjax('#main', {"fragment":"#main", "scrollTo":false});
 });
