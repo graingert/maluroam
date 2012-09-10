8475cbd19ffd06cc231814d637aa9722b03a7c99
Thomas Grainger <tagrain@gmail.com>
2012-09-10 14:28:12 +0100

    depend on django-angularjs for localy hosted lib

--- maluroam/settings.py
+++ maluroam/settings.py
@@ -128,6 +128,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'jquery',
+    'angularjs',
     'compressor',
     'maluroam.eduroam_snort',
     'crispy_forms',
